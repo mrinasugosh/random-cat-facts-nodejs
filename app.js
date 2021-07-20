ed6ca660230702a0cf42edb4a55251c5e85ac6c3
Mrinalini Sugosh <mrinalini24@berkeley.edu>
2021-07-19 22:19:17 -0400

    chore: add Dockerfile

--- app.js
+++ app.js
@@ -25,7 +25,26 @@ app.post('/', function (req, res) {
         res.render('index', {fact: null, error: 'Error, please try again'});
       } else {
         let randomFact = resp.fact;
+        console.log('Random Fact: ', randomFact);
         res.render('index', {fact: randomFact, error: null});
+      }
+    }
+  });
+})
+
+app.post('/test', function (req, res) {
+  let url = `https://catfact.ninja/fact`
+
+  request(url, function (err, response, body) {
+    if(err){
+      console.log('Error, please try again');
+    } else {
+      let resp = JSON.parse(body)
+      if(resp.fact == undefined){
+        console.log('Error with response, please try again');
+      } else {
+        let randomFact = resp.fact;
+        console.log('Random Fact: ', randomFact);
       }
     }
   });
